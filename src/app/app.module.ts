97d3b873ed02570c22f8904d89bbff374b024891
abhinav.ashish-kumar <abhinav.ashish-kumar@socgen.com>
2022-02-13 00:02:28 +0530

    web components using module federation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,15 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap() {
+    const ce = createCustomElement(AppComponent, {injector: this.injector});
+    customElements.define('angular1-element', ce);
+  }
+
+}
